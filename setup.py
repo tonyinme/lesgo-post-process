04a78cb11c9edb13d818b86da9e900d5acd98838
tonyinme <tonyinme@gmail.com>
2015-06-21 13:01:29 +0200

    Added lesgopp script

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ setup(
     author='Tony Martinez',
     author_email='tony.mtos@gmail.com',
     packages=['lesgo'],
-    scripts=['bin/cgns_to_vdf.py', 'bin/video_cgns_to_vdf.py'],
+    scripts=['bin/cgns_to_vdf.py', 'bin/video_cgns_to_vdf.py', 'bin/lesgopp'],
     url='http://pypi.python.org/pypi/lesgo/',
     license='LICENSE.txt',
     description='Post processing utilities for CGNS output.',
     long_description=open('README.txt').read(),
-    install_requires=["nose",
-        "numpy >= 1.8.0",
-        "h5py >= 2.0.0",
-    ],
-)
+#    install_requires=["nose", "numpy >= 1.8.0", "h5py >= 2.0.0",],
+    ) 
 
