41248ccf034816f60d3562e3401a4b841542b2a6
tonyinme <tonyinme@gmail.com>
2015-06-22 11:44:03 +0200

    Changed backend

--- lesgo/lesgo.py
+++ lesgo/lesgo.py
@@ -35,7 +35,11 @@ import interpolation as intp
 import numpy as np
 import os
 import h5py
+import matplotlib
+matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
+
+
 #~ from scipy.interpolate import griddata
 
 #############################
